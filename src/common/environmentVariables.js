582b4ab74d5de1a2c6665255b9632cd82045c7da
Juan Monroy <juan.monroy@dreamcodesoft.com>
2021-06-16 15:14:54 -0500

    deleted tradein

--- src/common/environmentVariables.js
+++ src/common/environmentVariables.js
@@ -3,6 +3,4 @@ exports.AWS_ENV=process.env.AWS_ENV;//aut
 exports.AWS_ENV2=process.env.AWS_ENV2;//auth
 exports.AWS_APP_NAME=process.env.AWS_APP_NAME;//bienestar
 exports.SERVICE_URL = process.env.SERVICE_URL;
-exports.APIKEY = process.env.APIKEY;
-exports.APIKEYS_OUTPUT_ENABLED = process.env.APIKEYS_OUTPUT_ENABLED;
-exports.APIGATEWAY_OUTPUT_ENABLED = process.env.APIGATEWAY_OUTPUT_ENABLED;//True
+exports.APIGATEWAY_OUTPUT_ENABLED = process.env.APIGATEWAY_OUTPUT_ENABLED;//"true"
